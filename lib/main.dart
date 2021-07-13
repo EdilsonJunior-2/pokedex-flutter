b88ca6e45ad88b57c9cba0dfa5487aa9b7133138
Edilson <juniorse13@gmail.com>
2021-07-13 17:32:31 -0300

    materialApp app name changed

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pokedex',
       theme: ThemeData(
       ),
       initialRoute: '/',
